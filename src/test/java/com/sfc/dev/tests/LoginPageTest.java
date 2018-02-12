cf98eb1af86887dd4f143b88be2ac184a029774e
suresh krishnan <sureshkrishnan@live.com>
2018-02-11 19:27:54 -0500

    version2

--- src/test/java/com/sfc/dev/tests/LoginPageTest.java
+++ src/test/java/com/sfc/dev/tests/LoginPageTest.java
@@ -1,51 +1,63 @@
 package com.sfc.dev.tests;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.sfc.dev.base.TestBase;
+import com.sfc.dev.pages.HomePage;
 import com.sfc.dev.pages.Loginpage;
 
-public class LoginPageTest extends TestBase
-{
+public class LoginPageTest extends TestBase {
 
 	Loginpage login;
-	public LoginPageTest(){
-		super(); 
+	HomePage homepage;
+
+	public LoginPageTest() {
+		super();
 	}
-	
+
 	@BeforeMethod
-	public void Setup(){
-		
+	public void Setup() {
+
 		initialization();
-		 login=new Loginpage();
-		
+		login = new Loginpage();
+
 	}
-	
-	@Test
-	public void loginpageTitleTest(){
-		
+
+	@Test(priority = 1)
+	public void loginpageTitleTest() {
+
 		System.out.println("Working flow");
-		
-		String Title=login.validateLoginPageTitle();
-		Assert.assertEquals( Title,"Grow Your Business With Canada's #1 Cloud-Based CRM System");
-		
-		//login.login(un, pwd);
+
+		String Title = login.validateLoginPageTitle();
+		Assert.assertEquals(Title, "Login | Salesforce");
+
+		// login.login(un, pwd);
 		//
-		
-		//login.login(prop.getProperty("username"), prop.getProperty("password"));
-		
+
+		// login.login(prop.getProperty("username"),
+		// prop.getProperty("password"));
+
 	}
-	
+
+	@Test(priority = 2)
+	public void loginpageLoginTest() {
+
+		homepage=login.login(prop.getProperty("username"), prop.getProperty("password"));
+
+	}
+
 	@AfterMethod
-	public void teardown(){
-		
-		//driver.quit();
-		
+	public void teardown() {
+
+		 driver.quit();
+
 	}
-	
-	
-	
+
 }
